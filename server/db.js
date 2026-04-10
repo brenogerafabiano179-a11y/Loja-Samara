92d2c9aba24c2f824673dc3af3f77ac0064e49c6
brenogerafabiano179-a11y <brenogerafabiano179@gmail.com>
2026-04-09 22:12:45 -0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,4 +1,7 @@
+const dns = require('dns');
 const { Pool } = require('pg');
+
+dns.setDefaultResultOrder('ipv4first');
 
 const connectionString = process.env.DATABASE_URL;
 
@@ -6,8 +9,13 @@ if (!connectionString) {
     throw new Error('DATABASE_URL não configurada.');
 }
 
+const shouldUseSsl = process.env.PGSSLMODE === 'require'
+    || /supabase\.co/i.test(connectionString)
+    || /sslmode=require/i.test(connectionString);
+
 const pool = new Pool({
-    connectionString
+    connectionString,
+    ssl: shouldUseSsl ? { rejectUnauthorized: false } : undefined
 });
 
 module.exports = {
